1ded0f6d743b1e90b6af3fbb24d0fc35f45b7b98
Alejandro Orozco Munoz <104179006+AlejandroOrozcoMunoz@users.noreply.github.com>
2022-09-12 23:16:49 -0500

    Add files via upload

---
+++ proyecto-grupal-main/flask_app/static/script/translate.js
@@ -0,0 +1,13 @@
+var translateButton = document.getElementById('translateIndex')
+translateButton.addEventListener("click", function(){
+    
+})
+
+async function translate(text) {
+    let res = await axios.post(
+    `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
+    { q: text, target: "tr" }
+    );
+    let translation = res.data.data.translations[0].translatedText;
+    return translation;
+    }
